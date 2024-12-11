be19d685927e7cf033157b9fd9eb60301b6f159d
Alex Mabry <alex.mabry@adtran.com>
2024-12-11 10:46:50 -0600

    feat: (day1) without numpy

--- aoc24/src/puzzles/day01.py
+++ aoc24/src/puzzles/day01.py
@@ -3,9 +3,18 @@ from ..utils import parse_data
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    walk_numbers = (tuple(int(x) for x in row.split("   ")) for row in input_data)
 
-    return None
+    left, right = [], []
+    for l, r in walk_numbers:
+        left.append(l)
+        right.append(r)
+
+    total_diff = 0
+    for l, r in zip(sorted(left), sorted(right)):
+        total_diff += abs(l - r)
+
+    return total_diff
 
 
 def solve_part2(data: str):
