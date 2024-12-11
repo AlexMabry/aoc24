f1a5673c27fbd08285f5c1d773972aa4b978c32c
Alex Mabry <alex.mabry@adtran.com>
2024-12-11 11:48:39 -0600

    feat: (day1) without numpy

--- aoc24/src/puzzles/day01.py
+++ aoc24/src/puzzles/day01.py
@@ -1,24 +1,35 @@
+from functools import reduce
+
+from black.trans import defaultdict
+
 from ..utils import parse_data
 
 
-def solve_part1(data: str):
-    input_data = parse_data(data)
+def get_left_right(input_data):
     walk_numbers = (tuple(int(x) for x in row.split("   ")) for row in input_data)
-
     left, right = [], []
+
     for l, r in walk_numbers:
         left.append(l)
         right.append(r)
 
-    total_diff = 0
-    for l, r in zip(sorted(left), sorted(right)):
-        total_diff += abs(l - r)
+    return left, right
 
-    return total_diff
+def solve_part1(data: str):
+    input_data = parse_data(data)
+    left, right = get_left_right(input_data)
+
+    return sum([abs(l - r) for l, r in zip(sorted(left), sorted(right))])
+
+
+def count_appearance(d, n):
+    d[n] += 1
+    return d
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    left, right = get_left_right(input_data)
 
-    return None
+    count = reduce(count_appearance, right, defaultdict(int))
+    return sum([num*count[num] for num in left])
