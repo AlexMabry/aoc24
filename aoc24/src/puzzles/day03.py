1aefdb0c6ed8ab150b4c05b3c652a41dd6681d22
Alex Mabry <mabryecom@gmail.com>
2024-12-25 15:36:03 -0600

    feat: (day3) not bad

--- aoc24/src/puzzles/day03.py
+++ aoc24/src/puzzles/day03.py
@@ -1,15 +1,35 @@
+import re
 from ..utils import parse_data
+
+PATTERN = re.compile(r"mul\((?P<a>\d{1,3}),(?P<b>\d{1,3})\)|(?P<yes>do)\(\)|(?P<no>don't)\(\)")
+
+
+def scan_data(data: list[str]):
+    for line in data:
+        for result in PATTERN.finditer(line):
+            yield result.groupdict()
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    answer = 0
+    for result in scan_data(input_data):
+        if result["a"] and result["b"]:
+            answer += int(result["a"]) * int(result["b"])
 
-    return None
+    return answer
+
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    answer, enabled = 0, True
+    for result in scan_data(input_data):
+        if not enabled and result["yes"]:
+            enabled = True
+        elif enabled and result["no"]:
+            enabled = False
+        elif enabled and result["a"] and result["b"]:
+            answer += int(result["a"]) * int(result["b"])
 
-    return None
+    return answer
