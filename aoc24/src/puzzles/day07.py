2baf90317c24a401911a86cc5d79d2ccf3ee96cb
Alex Mabry <alex.mabry@adtran.com>
2024-12-30 15:38:04 -0600

    feat: (day 7) not very pretty

--- aoc24/src/puzzles/day07.py
+++ aoc24/src/puzzles/day07.py
@@ -1,15 +1,50 @@
 from ..utils import parse_data
 
 
+def parse_problems(input_data):
+    problems = []
+    for line in input_data:
+        value, numbers = line.split(": ")
+        problems.append((int(value), [int(x) for x in numbers.split(" ")]))
+    return problems
+
+
+def is_valid_1(value, numbers):
+    if len(numbers) == 1:
+        return value == numbers[0]
+    last = numbers.pop()
+    possibilities = [is_valid_1(value - last, list(numbers))]
+    if value % last == 0:
+        possibilities.append(is_valid_1(value // last, list(numbers)))
+
+    return any(possibilities)
+
+
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    problems = parse_problems(input_data)
 
-    return None
+    return sum([value for value, numbers in problems if is_valid_1(value, numbers)])
+
+
+def is_valid_2(value, numbers):
+    if value <= 0:
+        return False
+    if len(numbers) == 1:
+        return value == numbers[0]
+    last = numbers.pop()
+    possibilities = [is_valid_2(value - last, list(numbers))]
+    if value % last == 0:
+        possibilities.append(is_valid_2(value // last, list(numbers)))
+    if str(value).endswith(str(last)) and len(str(value)) > len(str(last)):
+        value = int(str(value)[: -len(str(last))])
+        possibilities.append(is_valid_2(value, list(numbers)))
+
+    return any(possibilities)
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    problems = parse_problems(input_data)
 
-    return None
+    return sum([value for value, numbers in problems if is_valid_2(value, numbers)])
