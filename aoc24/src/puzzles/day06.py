1e24e959b15dfaedad7b15e93fdf6d4167cb0107
Alex Mabry <alex.mabry@adtran.com>
2024-12-30 14:46:00 -0600

    feat: (day 6) lots of code

--- aoc24/src/puzzles/day06.py
+++ aoc24/src/puzzles/day06.py
@@ -1,15 +1,63 @@
 from ..utils import parse_data
 
+N, E, S, W = (0, -1), (1, 0), (0, 1), (-1, 0)
+TURN = {N: E, E: S, S: W, W: N}
+
+
+def look_ahead(guard, direction):
+    return guard[0] + direction[0], guard[1] + direction[1]
+
+
+def parse_floor(input_data):
+    floor, obstructions, location = set(), set(), None
+    for y, row in enumerate(input_data):
+        for x, c in enumerate(row):
+            if c == "^":
+                location = (x, y)
+            if c != "#":
+                floor.add((x, y))
+            else:
+                obstructions.add((x, y))
+
+    return floor, obstructions, location
+
+
+def create_path(floor, obstructions, location, direction=N):
+    path = set()
+    while location in floor:
+        if (location, direction) in path:
+            return None  # Loop detected
+
+        path.add((location, direction))
+        if (next_step := look_ahead(location, direction)) in obstructions:
+            direction = TURN[direction]
+        else:
+            location = next_step
+
+    return path
+
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    floor, obstructions, location = parse_floor(input_data)
+    path = create_path(floor, obstructions, location)
 
-    return None
+    return len({location for location, _ in path})
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    floor, obstructions, guard = parse_floor(input_data)
+    path = create_path(floor, obstructions, guard)
 
-    return None
+    new_obstructions = set()
+    for location, direction in path:
+        next_step = look_ahead(location, direction)
+        if (
+            next_step in floor
+            and next_step not in new_obstructions
+            and not create_path(floor - {next_step}, obstructions | {next_step}, guard)
+        ):
+            new_obstructions.add(next_step)
+
+    return len(new_obstructions)
